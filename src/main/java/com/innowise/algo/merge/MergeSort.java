5ee2417ced8f8c8bd04be49d4d16c655772761c0
Nikolai Urusov <nikolai.urusov@gmail.com>
2023-03-05 22:34:26 +0300

    add MergeSort.java and ArrayUtil.java

---
+++ src/main/java/com/innowise/algo/merge/MergeSort.java
@@ -0,0 +1,23 @@
+package com.innowise.algo.merge;
+
+import com.innowise.algo.BoolComparator;
+
+public interface MergeSort<T> {
+    BoolComparator<T> comparator();
+
+
+    default void merge(T[] trg, T[] left, T[] right) {
+        int l = 0, r = 0, i = 0;
+        while (l < left.length && r < right.length) {
+            trg[i++] = comparator().compare(right[r], left[l]) ? left[l++] : right[r++];
+        }
+
+        while (l < left.length) {
+            trg[i++] = left[l++];
+        }
+
+        while (r < right.length) {
+            trg[i++] = right[r++];
+        }
+    }
+}
