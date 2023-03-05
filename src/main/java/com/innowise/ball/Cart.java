7da61e77a0bf8d00ee6e790cbf310cbca1872a74
Nikolai Urusov <nikolai.urusov@gmail.com>
2023-03-05 22:58:38 +0300

    change Cart.java

--- src/main/java/com/innowise/ball/Cart.java
+++ src/main/java/com/innowise/ball/Cart.java
@@ -1,15 +1,37 @@
 package com.innowise.ball;
 
+import com.innowise.algo.BoolComparator;
+import com.innowise.algo.Sort;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 
 public class Cart {
 
     private Collection<Ball> balls;
 
+    private Sort<Ball> strategy = Sort.defaultBallSort();
+
     public Cart() {
         this.balls = new ArrayList<>();
     }
+
+
+    public void sort(BoolComparator<Ball> newComparator) {
+        strategy.setComparator(newComparator);
+        this.sort();
+    }
+
+
+    public void sort() {
+        Ball[] arr = balls.toArray(Ball[]::new);
+
+        strategy.sort(arr);
+
+        balls = new ArrayList<>(Arrays.stream(arr).toList());
+    }
+
 
     public Cart(Collection<Ball> balls) {
         this.balls = balls;
@@ -38,6 +60,10 @@ public class Cart {
                 '}';
     }
 
+
+    public void setSort(Sort<Ball> sort) {
+        this.strategy = sort;
+    }
 
     public Collection<Ball> getBalls() {
         return balls;
