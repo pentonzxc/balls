77d105fb60970d1deb443dd8ffcddc76612ea7a3
Nikolai Urusov <nikolai.urusov@gmail.com>
2023-03-05 22:27:17 +0300

    add AbstractSort.class

---
+++ src/main/java/com/innowise/algo/AbstractSort.java
@@ -0,0 +1,32 @@
+package com.innowise.algo;
+
+import java.util.concurrent.RecursiveAction;
+
+abstract public class AbstractSort<T> extends RecursiveAction implements Sort<T> {
+
+    protected BoolComparator<T> comp;
+
+    protected Class<T> token;
+
+
+    public AbstractSort(BoolComparator<T> comp) {
+        this.comp = comp;
+    }
+
+    public void sort(T[] arr) {
+        setToken(arr.getClass());
+        doSort(arr);
+    }
+
+    protected abstract void doSort(T[] src);
+
+
+    @SuppressWarnings("unchecked")
+    protected void setToken(Class<?> tokenClazz) {
+        this.token = (Class<T>) tokenClazz.getComponentType();
+    }
+
+    public void setComparator(BoolComparator<T> comparator) {
+        this.comp = comparator;
+    }
+}
